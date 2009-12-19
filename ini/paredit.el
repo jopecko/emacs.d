bed8cb65ee7212f7e1033e2743ddbe9c60cb93f9
Joe O'Pecko <opeckojo@gmail.com>
2009-12-19 09:20:39 -0500

    removed commented out code

--- ini/paredit.el
+++ ini/paredit.el
@@ -6,10 +6,6 @@
 
 (when (require-soft 'paredit)
   (show-paren-mode t)
-  ;;(define-key paredit-mode-map (kbd ")")
-  ;;'paredit-close-parenthesis)
-  ;; (define-key paredit-mode-map (kbd "M-)")
-  ;;   'paredit-close-parenthesis-and-newline))
   (dolist (x '(scheme emacs-lisp lisp clojure slime slime-repl inferior-lisp))
     (add-hook
      (intern (concat (symbol-name x) "-mode-hook"))
